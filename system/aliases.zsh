a97bd251eab4b5ad530efdfc5b6ae7a3d35f06c7
Ryan King <hello@ryanking.com>
2016-10-11 23:17:26 -0400

    Update spacemacs and zshenv for ATS

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -6,3 +6,9 @@ then
     alias la='gls -A --color'
     alias vim='nvim'
 fi
+
+# Classes
+alias BUCS="cd $HOME/Projects/Boston-University-CS"
+alias CS520="cd $HOME/Projects/Boston-University-CS/CS520\ -\ Principles\ of\ Programming\ Languages"
+alias CS655="cd $HOME/Projects/Boston-University-CS/CS655\ -\ Computer\ Networks"
+alias CS680="cd $HOME/Projects/Boston-University-CS/CS680\ -\ Introduction\ to\ Computer\ Graphics"
