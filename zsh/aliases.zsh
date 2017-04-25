c8e5fa067824a62c848c12b174ff3ad5c8250266
Ryan King <hello@ryanking.com>
2017-04-25 18:58:02 -0400

    Changing very very minorly

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,2 +1,4 @@
 alias reload!='. ~/.zshrc'              # Reload zsh config
 alias cls='clear'                       # Clear Screeh
+
+alias CS552='cd ~/Projects/Boston-University-CS/CS552\ -\ Operating\ Systems'
